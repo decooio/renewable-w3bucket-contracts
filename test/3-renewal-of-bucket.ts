bd15b96738b21f9ca533340d66707adfdc486d56
eericxu <2681350846@qq.com>
2022-12-09 11:11:58 +0800

    renewal test

--- test/3-renewal-of-bucket.ts
+++ test/3-renewal-of-bucket.ts
@@ -3,39 +3,58 @@ import { expect } from "chai";
 import { deployRenewableW3BucketWithMinted, mapRenewal, BucketRenewalStruct } from "./utils";
 
 describe("Renewal of bucket", () => {
-  
   it("renew is work", async () => {
     const {
       w3Bucket,
       Alice,
+      testERC20,
       tokenIds: [tokenId],
-      prices,
+      prices: [eth, erc20],
     } = await deployRenewableW3BucketWithMinted();
-    
-    const price = prices[0].price.mul(2).mul(2);
+
+    const price = eth.price.mul(2).mul(2);
     // emit BucketRenewed(tokenId, currency, unitPrice, capacityUnits, periodUnits, _msgSender())
-    await expect(w3Bucket.connect(Alice).renewBucket(tokenId, prices[0].currency, 2, 2, { value: price }))
+    // eth
+    await expect(w3Bucket.connect(Alice).renewBucket(tokenId, eth.currency, 2, 2, { value: price }))
       .to.emit(w3Bucket, "BucketRenewed")
-      .withArgs(tokenId, prices[0].currency, prices[0].price, 2, 2, Alice.address)
+      .withArgs(tokenId, eth.currency, eth.price, 2, 2, Alice.address)
       .to.changeEtherBalances([w3Bucket.address, Alice.address], [price, price.mul(-1)]);
-    expect(await w3Bucket.bucketRenewalCount(tokenId)).to.equal(2, "count is 2");
+    // erc20
+    const erc20Price = erc20.price.mul(2).mul(2);
+    await expect(testERC20.connect(Alice).approve(w3Bucket.address, erc20Price)).to.eventually.be.fulfilled
+    await expect(w3Bucket.connect(Alice).renewBucket(tokenId, erc20.currency, 2, 2))
+      .to.emit(w3Bucket, "BucketRenewed")
+      .withArgs(tokenId, erc20.currency, erc20.price, 2, 2, Alice.address)
+      .to.changeTokenBalances(testERC20, [w3Bucket.address, Alice.address], [erc20Price, erc20Price.mul(-1)]);
+    // check count
+    await expect(w3Bucket.bucketRenewalCount(tokenId)).to.eventually.equal(3, "count is 3");
+    // check renewals
     const r0: Partial<BucketRenewalStruct> = {
       tokenId,
-      currency: prices[0].currency,
-      unitPrice: prices[0].price,
+      currency: eth.currency,
+      unitPrice: eth.price,
       capacityUnits: BigNumber.from(1),
       periodUnits: BigNumber.from(1),
       renewedBy: Alice.address,
     };
-    expect(await w3Bucket.renewalOfBucketByIndex(tokenId, 0).then(mapRenewal)).to.deep.include(r0);
+    await expect(w3Bucket.renewalOfBucketByIndex(tokenId, 0).then(mapRenewal)).to.eventually.deep.include(r0);
     const r1: Partial<BucketRenewalStruct> = {
       tokenId,
-      currency: prices[0].currency,
-      unitPrice: prices[0].price,
+      currency: eth.currency,
+      unitPrice: eth.price,
       capacityUnits: BigNumber.from(2),
       periodUnits: BigNumber.from(2),
       renewedBy: Alice.address,
     };
-    expect(await w3Bucket.renewalOfBucketByIndex(tokenId, 1).then(mapRenewal)).to.deep.include(r1);
+    await expect(w3Bucket.renewalOfBucketByIndex(tokenId, 1).then(mapRenewal)).to.eventually.deep.include(r1);
+    const r2: Partial<BucketRenewalStruct> = {
+      tokenId,
+      currency: erc20.currency,
+      unitPrice: erc20.price,
+      capacityUnits: BigNumber.from(2),
+      periodUnits: BigNumber.from(2),
+      renewedBy: Alice.address,
+    };
+    await expect(w3Bucket.renewalOfBucketByIndex(tokenId, 2).then(mapRenewal)).to.eventually.deep.include(r2);
   });
 });
